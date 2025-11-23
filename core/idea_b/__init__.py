237c3ff3292b8194fceb0bbc773361d86efbc356
Chetan Patil <71105582+chetanxpatil@users.noreply.github.com>
2025-11-23 22:34:39 +0530

    Remove idea_b and internet folders
    - Deleted core/idea_b (teleportation protocol design)
    - Deleted core/internet (real-time connection implementation)
    - Cleanup: Removing experimental/prototype code

--- core/idea_b/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Idea B: Simulated Teleportation Using Livnium Cores
-
-Implements a simulated quantum teleportation protocol using Livnium cores,
-mimicking the information-theoretic structure of quantum teleportation.
-"""
-
-__all__ = []
-
